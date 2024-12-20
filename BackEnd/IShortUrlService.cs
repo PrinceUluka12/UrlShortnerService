56fab4411da435e0309965b756113a9b1bdf5d4b
Prince <prince.uluka@icloud.com>
2024-12-20 18:36:42 -0500

    Updates

--- BackEnd/IShortUrlService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Threading.Tasks;
-
-public interface IShortUrlService
-{
-    string GenerateShortUrl(string longUrl, int length);
-    Task<ShortUrl> CreateShortUrlAsync(string userName, string longUrl, int length);
-    Task<string> GetLongUrlAsync(string shortUrlCode);
-}
--- BackEnd/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ BackEnd/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3611848e22c77dcdcb0ada76b86c3cc0e04c98a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
